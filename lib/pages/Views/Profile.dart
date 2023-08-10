5c2e3b4e7eecd99fb8f83593a853796d1546d575
priya-mohod <contact.priyamohod@gmail.com>
2023-08-10 21:21:17 +0100

    UserProfile Class created

---
+++ lib/pages/Views/Profile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class UserProfile extends StatefulWidget {
+  const UserProfile({super.key});
+
+  @override
+  State<UserProfile> createState() => _UserProfileState();
+}
+
+class _UserProfileState extends State<UserProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
