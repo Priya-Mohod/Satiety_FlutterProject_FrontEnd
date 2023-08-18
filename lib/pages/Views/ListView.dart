a6905284169eb8eac6e99e6697246acc180eb017
priya-mohod <contact.priyamohod@gmail.com>
2023-08-18 01:43:17 +0100

    fixing sum bugs

--- lib/pages/Views/ListView.dart
+++ lib/pages/Views/ListView.dart
@@ -161,148 +161,143 @@ class _ListViewPageState extends State<ListViewPage> {
               builder: (context, foodListViewModel, child) {
                 final foodList = foodListViewModel.foodList;
                 return ListView.builder(
-                  shrinkWrap: true,
                   itemCount: foodList.length,
                   itemBuilder: (context, index) {
                     final foodItem = foodList[index];
-                    return Padding(
-                      padding: const EdgeInsets.only(top: 5),
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(
-                              10.0), // Adjust the value as needed
-                          side: const BorderSide(
-                              color: Color.fromARGB(255, 128, 172, 177),
-                              width: 1.0), // Add border color and width
-                        ),
-                        elevation: 4,
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      FoodDetails(foodItem: foodList[index]),
-                                ));
-                          },
-                          child: Row(
-                            children: [
-                              Image.network(
-                                foodItem.foodSignedUrl,
-                                height: 150,
-                                width: 130,
-                                errorBuilder: (context, error, stackTrace) {
-                                  return Image.asset(
-                                    'images/a.png',
-                                    height: 100,
-                                    width: 100,
-                                  );
-                                },
-                              ),
-                              SizedBox(width: 15),
-                              Row(
-                                children: [
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      SizedBox(height: 15),
-                                      Text(foodItem.foodName,
-                                          style: TextStyle(
-                                            fontSize: 26,
-                                            fontWeight: FontWeight.bold,
-                                          )),
-                                      SizedBox(height: 5),
-                                      Row(
-                                        children: [
-                                          ClipOval(
-                                            child: Icon(Icons.account_circle,
-                                                size: 40,
-                                                color: Colors.red[300]),
-                                          ),
-                                          SizedBox(width: 3),
-                                          Text(foodItem.addedByUserName,
-                                              style: TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.grey[700],
-                                              )),
-                                          SizedBox(width: 10),
-                                          Icon(
-                                            Icons.star_half,
-                                            size: 30,
-                                            color: Colors.orange[800],
-                                          ),
-                                          SizedBox(width: 5),
-                                          Text('3.5',
-                                              style: TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.grey[800],
-                                              )),
-                                        ],
-                                      ),
-                                      Row(
-                                        children: [
-                                          Icon(Icons.location_on_outlined,
-                                              size: 30,
-                                              color: Colors.grey[700]),
-                                          SizedBox(width: 3),
-                                          Text('1.1 km',
-                                              style: TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.grey[700],
-                                              )),
-                                          // Text(
-                                          //     //'Amount: ${foodItem.foodAmount.toStringAsFixed(1)}'
-                                          //     foodItem.foodAmount == 0.0
-                                          //         ? 'Free'
-                                          //         : 'Cost: Rs. ${foodItem.foodAmount}',
-                                          //     style: TextStyle(
-                                          //       fontSize: 20,
-                                          //       //color: Colors.black54,
-                                          //       color:
-                                          //           foodItem.foodAmount == 0.0
-                                          //               ? Color.fromARGB(
-                                          //                   255, 40, 125, 43)
-                                          //               : Colors.black87,
-                                          //       fontWeight: FontWeight.bold,
-                                          //     )),
+                    return Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            10.0), // Adjust the value as needed
+                        side: const BorderSide(
+                            color: Color.fromARGB(255, 128, 172, 177),
+                            width: 0.5), // Add border color and width
+                      ),
+                      elevation: 4,
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    FoodDetails(foodItem: foodList[index]),
+                              ));
+                        },
+                        child: Row(
+                          children: [
+                            Image.network(
+                              foodItem.foodSignedUrl,
+                              height: 150,
+                              width: 130,
+                              errorBuilder: (context, error, stackTrace) {
+                                return Image.asset(
+                                  'images/a.png',
+                                  height: 100,
+                                  width: 100,
+                                );
+                              },
+                            ),
+                            SizedBox(width: 15),
+                            Row(
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 15),
+                                    Text(foodItem.foodName,
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                          fontSize: 26,
+                                          fontWeight: FontWeight.bold,
+                                        )),
+                                    SizedBox(height: 5),
+                                    Row(
+                                      children: [
+                                        ClipOval(
+                                          child: Icon(Icons.account_circle,
+                                              size: 40, color: Colors.red[300]),
+                                        ),
+                                        SizedBox(width: 3),
+                                        Text(foodItem.addedByUserName,
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.grey[700],
+                                            )),
+                                        SizedBox(width: 10),
+                                        Icon(
+                                          Icons.star_half,
+                                          size: 30,
+                                          color: Colors.orange[800],
+                                        ),
+                                        SizedBox(width: 5),
+                                        Text('3.5',
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.grey[800],
+                                            )),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Icon(Icons.location_on_outlined,
+                                            size: 30, color: Colors.grey[700]),
+                                        SizedBox(width: 3),
+                                        Text('1.1 km',
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.grey[700],
+                                            )),
+                                        // Text(
+                                        //     //'Amount: ${foodItem.foodAmount.toStringAsFixed(1)}'
+                                        //     foodItem.foodAmount == 0.0
+                                        //         ? 'Free'
+                                        //         : 'Cost: Rs. ${foodItem.foodAmount}',
+                                        //     style: TextStyle(
+                                        //       fontSize: 20,
+                                        //       //color: Colors.black54,
+                                        //       color:
+                                        //           foodItem.foodAmount == 0.0
+                                        //               ? Color.fromARGB(
+                                        //                   255, 40, 125, 43)
+                                        //               : Colors.black87,
+                                        //       fontWeight: FontWeight.bold,
+                                        //     )),
 
-                                          SizedBox(width: 10),
-                                          if (foodItem.foodType == 'Veg')
-                                            const Icon(Icons.fastfood,
-                                                color: Color.fromARGB(
-                                                    255, 40, 125, 43),
-                                                size: 25),
-                                          if (foodItem.foodType == 'Non-Veg')
-                                            const Icon(Icons.fastfood,
-                                                color: Colors.red, size: 25),
-                                          if (foodItem.foodType == 'Both')
-                                            const Icon(Icons.fastfood,
-                                                color: Colors.orange, size: 25),
-                                          SizedBox(width: 20),
-                                          if (foodItem.foodAmount == 0.0)
-                                            Icon(
-                                              Icons.currency_rupee_rounded,
-                                              size: 30,
-                                              color: Colors.green[800],
-                                            ),
-                                          if (foodItem.foodAmount != 0.0)
-                                            Icon(
-                                              Icons.currency_rupee_rounded,
-                                              size: 30,
-                                              color: Colors.red[900],
-                                            )
-                                        ],
-                                      )
-                                    ],
-                                  )
-                                ],
-                              )
-                            ],
-                          ),
+                                        SizedBox(width: 10),
+                                        if (foodItem.foodType == 'Veg')
+                                          const Icon(Icons.fastfood,
+                                              color: Color.fromARGB(
+                                                  255, 40, 125, 43),
+                                              size: 25),
+                                        if (foodItem.foodType == 'Non-Veg')
+                                          const Icon(Icons.fastfood,
+                                              color: Colors.red, size: 25),
+                                        if (foodItem.foodType == 'Both')
+                                          const Icon(Icons.fastfood,
+                                              color: Colors.orange, size: 25),
+                                        SizedBox(width: 20),
+                                        if (foodItem.foodAmount == 0.0)
+                                          Icon(
+                                            Icons.currency_rupee_rounded,
+                                            size: 30,
+                                            color: Colors.green[800],
+                                          ),
+                                        if (foodItem.foodAmount != 0.0)
+                                          Icon(
+                                            Icons.currency_rupee_rounded,
+                                            size: 30,
+                                            color: Colors.red[900],
+                                          )
+                                      ],
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
                         ),
                       ),
                     );
