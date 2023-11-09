488fb736d36dbc130c0ec8e426dea7799dee8283
Tushar Mohod <tushmohod5397@gmail.com>
2023-11-09 01:09:31 +0000

    Worked on Login and OTP screen

---
+++ lib/pages/Constants/ColorConstants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const Color buttonColor = Colors.blue;
+  static const Color primaryColor = Colors.cyanAccent;
+}
---
+++ lib/pages/Screens/VerifyOTPScreen.dart
@@ -0,0 +1,117 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
+import 'package:satietyfrontend/pages/Constants/ColorConstants.dart';
+import 'package:satietyfrontend/pages/Views/SnackbarHelper.dart';
+import 'package:satietyfrontend/pages/Views/Widgets/CustomButton.dart';
+
+class VerifyOTPScreen extends StatefulWidget {
+  const VerifyOTPScreen({super.key});
+
+  @override
+  State<VerifyOTPScreen> createState() => _VerifyOTPScreenState();
+}
+
+class _VerifyOTPScreenState extends State<VerifyOTPScreen> {
+  String? otpCode;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+              child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Icon(CupertinoIcons.back),
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              // padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle, color: ThemeColors.primaryColor),
+              // Replace 'banner.jpg' with your image asset
+              child: Image.asset('images/SatietyLogo.png'),
+            ),
+            const SizedBox(height: 20),
+            const Text('Verification',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
+            const Text(
+              'Enter the OTP you received',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
+            ),
+            const SizedBox(height: 20),
+            Pinput(
+              length: 6,
+              showCursor: true,
+              defaultPinTheme: PinTheme(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: ThemeColors.primaryColor),
+                ),
+                textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+              onSubmitted: (value) {
+                setState(() {
+                  // *** assign to otpCode = value
+                  otpCode = value;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: CustomButton(
+                  text: 'Verify',
+                  onPressed: () {
+                    if (otpCode != null) {
+                      verfiyOTP(context, otpCode!);
+                    } else {
+                      SnackbarHelper.showSnackBar(
+                          context, 'Enter complete code');
+                    }
+                  }),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Didn't receive any code?",
+              style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black38),
+            ),
+            const SizedBox(height: 10),
+            GestureDetector(
+              onTap: () {
+                // *** Resend the code
+              },
+              child: const Text(
+                "Resend",
+                style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 6, 17, 17)),
+              ),
+            ),
+          ],
+        ),
+      ))),
+    );
+  }
+
+  void verfiyOTP(BuildContext context, String optCode) {}
+}
